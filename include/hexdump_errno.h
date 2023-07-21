7e2a0ca720ca4b0f66776773168bb8f819e66c5f
KeithBrown39423 <keithbrown39423@outlook.com>
2023-07-21 12:35:14 -0500

    :art: Create a header for exit codes

---
+++ include/hexdump_errno.h
@@ -0,0 +1,9 @@
+#ifndef _HEXDUMP_ERRNO_H
+#define _HEXDUMP_ERRNO_H
+
+#define HEX_EMISARG 1   // Missing argument
+#define HEX_EIO 2       // I/O error
+#define HEX_EFBIG 3     // File too big
+#define HEX_EFEMPTY 4   // File is empty
+
+#endif
