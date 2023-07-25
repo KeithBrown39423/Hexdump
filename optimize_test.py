d0894347475d2fde8ce56bb4f950cd3d0fee4643
Dumb Bird <72983221+ZackeryRSmith@users.noreply.github.com>
2023-07-25 11:59:44 -0400

    Crude feature addition and bug fixes (#23)
    Verbose logging makes this code hard to read, but having verbose logging is nice. I may remove it in the near future though, just to make the code smaller and easier to read. Regardless, added more INFO logs, fixed a bug with verbose logging, and added a `--avg-runtime`

--- optimize_test.py
+++ optimize_test.py
@@ -1,7 +1,11 @@
 from time import time
 import subprocess as sp
-from os import stat, path, remove, listdir, devnull
+from os import stat, path, remove, listdir, devnull 
 import sys
+from concurrent.futures import ProcessPoolExecutor, as_completed
+
+# TODO: refactor code flow (mainly with test execution)
+#       possibly remove verbose logging? (cleans code)
 
 # empty unless colorama is installed
 ERROR_COLOR = ''
@@ -27,8 +31,7 @@ except ImportError:
 
 from argparse import ArgumentParser, HelpFormatter
 
-# TODO: error checking
-#       work off abs not rel paths
+# TODO: work off abs not rel paths
 
 # get some basic info on this python file
 pyfile_name = path.basename(__file__)
@@ -60,9 +63,10 @@ file_handling = parser.add_argument_group("File Handling")
 test_execution = parser.add_argument_group("Test Execution")
 control_and_output = parser.add_argument_group("Control and Output")
 
-#--==--#
+#-=-=-==-==-=-=-#
+# FILE HANDLING #
+#-=-=-==-==-=-=-#
 # FILE #
-#--==--#
 file_handling.add_argument(
     "-f", "--file",
     metavar="FILE",
@@ -71,9 +75,7 @@ file_handling.add_argument(
     help="File to read bytes from during binary tests (defaults to this file)"
 )
 
-#--==--#
-# LOG! #
-#--==--#
+# LOG #
 file_handling.add_argument(
     "-l", "--log",
     metavar="LOG",
@@ -82,9 +84,7 @@ file_handling.add_argument(
     help="Output file that tests are stored to (default: ./test_results.txt)"
 )
 
-#--==--==--==--#
-#  BINARY OUT  #
-#--==--==--==--#
+# BINARY OUT #
 file_handling.add_argument(
     "-b", "--bin-out",
     metavar="PATH",
@@ -93,18 +93,10 @@ file_handling.add_argument(
     help="Place to store test binary outputs (default: ./bin/test)"
 )
 
-#--==--==--#
-#  CLEAN!  #
-#--==--==--#
-control_and_output.add_argument(
-    "--clean",
-    action="store_true",
-    help="Delete all test binaries"
-)
-
-#--==--==--#
+#--==--==--==--==--#
+#  TEST EXECUTION  #
+#--==--==--==--==--#
 # COMP AVG #
-#--==--==--#
 test_execution.add_argument(
     "--compavg",
     metavar="N",
@@ -113,63 +105,99 @@ test_execution.add_argument(
     help="Compute the average of all N tests"
 )
 
-#--==--==--#
+# AVG RUNTIME #
+test_execution.add_argument(
+    "--avg-runtime",
+    metavar="N",
+    action="store",
+    type=int,
+    help="Compute the average runtimes over N tests (compiles hexdump binaries asynchronously)"
+)
+
 # TEST ALL #
-#--==--==--#
 test_execution.add_argument(
     "-t", "--test-all",
     action="store_true",
     help="Test all optimizations"
 )
 
-#--==--==--#
-#  SILENT  #
-#--==--==--#
+#--==--==--==--==--==--#
+#  CONTROL AND OUTPUT  #
+#--==--==--==--==--==--#
+# SILENT #
 control_and_output.add_argument(
     "--silent",
     action="store_true",
     help="Redirect stdout to DEVNULL (supress output)"
 )
 
-#--==--==--==--#
-# VERBOSE MODE #
-#--==--==--==--#
+# VERBOSE #
 control_and_output.add_argument(
     "--verbose",
     action="store_true",
     help="Display detailed processing information"
 )
 
+# CLEAN #
+control_and_output.add_argument(
+    "--clean",
+    action="store_true",
+    help="Delete all test binaries"
+)
+
 # parse args
 args = parser.parse_args()
-###########################
-####  END OF ARGPARSE  ####
-###########################
+
+#############
+# Constants #
+#############
+ERROR = 3
+WARNING = 2
+VERBOSE = 1
+INFO = 0
+
+PYFILE_NAME = path.basename(__file__)
+PYFILE_DIR = path.dirname(__file__)
+
+BASE_COMPILE_COMMAND = ["g++", "-Wall", "-Werror", "-Wpedantic", "-I",
+                        "lib", "-I", "include", "src/hexdump.cpp", "-o"]
+OPTIMIZATION_LEVELS  = ["0", "1", "2", "3", "s", "fast", "g"]
+TEST_ARGS            = ["--ascii", args.file]
+
+###########
+# LOGGING #
+###########
+def log(type: int, *posargs, **kwargs):
+    # don't output verbose logs if flag isn't set
+    if type == VERBOSE and not args.verbose: return
+    
+    status = ""
+    if   type == INFO    : status = f"[{INFO_COLOR}INFO{RESET_COLOR}]"
+    elif type == VERBOSE : status = f"[{VERBOSE_COLOR}VERBOSE{RESET_COLOR}]"
+    elif type == WARNING : status = f"[{WARNING_COLOR}WARNING{RESET_COLOR}]"
+    elif type == ERROR   : status = f"[{ERROR_COLOR}ERROR{RESET_COLOR}]"
+    else                 : status = "[UNKNOWN]"
+    print(status, *posargs, **kwargs)
+
+
+#######################
+# arg integrity check #
+#######################
+if not path.exists(args.file):
+    log(ERROR, f"{args.file} DOES NOT EXIST!")
+    sys.exit(1)
+
+if not path.exists(args.bin_out):
+    log(ERROR, f"{args.bin_out} DOES NOT EXIST!")
+    sys.exit(1)
 
 if args.silent:
     f = open(devnull, 'w')
     sys.stdout = f
 
-base_command = ["g++", "-Wall", "-Werror", "-Wpedantic", "-I", "lib", "-I", "include", "src/hexdump.cpp", "-o"]
-optimization = ["0", "1", "2", "3", "s", "fast", "g"]
-test_args    = ["--ascii", args.file]
-
-ERROR   = 3
-WARNING = 2
-VERBOSE = 1
-INFO    = 0
-
-def log(type: int, *posargs, **kwargs):
-    if type == INFO:
-        print(f"[{INFO_COLOR}INFO{RESET_COLOR}]", *posargs, **kwargs)
-    elif type == VERBOSE and args.verbose:
-        print(f"[{VERBOSE_COLOR}VERBOSE{RESET_COLOR}]", *posargs, **kwargs)
-    elif type == WARNING:
-        print(f"[{WARNING_COLOR}WARNING{RESET_COLOR}]", *posargs, **kwargs)
-    elif type == ERROR:
-        print(f"[{ERROR_COLOR}ERROR{RESET_COLOR}]", *posargs, **kwargs)
-    else:
-        print("[UNKNOWN]", *posargs, **kwargs)
+###########################
+####  END OF ARGPARSE  ####
+###########################
 
 #==--         --==#
 # HEXDUMP VERSION #
@@ -184,55 +212,6 @@ def hexdump_ver() -> str:
                 return line[ver_start:ver_end]
 
 
-#==--  --==#
-# RUN TEST #
-#==--  --==#
-def run_test(opt, base_command=base_command, test_args=test_args) -> dict:
-    file_name = path.join(args.bin_out, f"hexdump-{opt}")
-    log(VERBOSE, f"file_name = {file_name}")
-
-    #----------------#
-    # COMPILE BINARY #
-    #----------------#
-    compile_command = base_command + [file_name, f"-O{opt}"]
-    log(VERBOSE, f"compile_command = {compile_command}")
-    t1 = time()
-    log(VERBOSE, "running compile_command")
-    sp.call(compile_command, stdout=sp.DEVNULL, stderr=sp.STDOUT)
-    log(VERBOSE, "compile_command executed")
-    t2 = time()
-
-    #------------#
-    # RUN BINARY #
-    #------------#
-    run_command = [file_name] + test_args
-    log(VERBOSE, f"run_command = {run_command}")
-    t3 = time()
-    log(VERBOSE, "running run_command")
-    sp.call(run_command, stdout=sp.DEVNULL, stderr=sp.STDOUT)
-    log(VERBOSE, "run_command executed")
-    t4 = time()
-    
-    log(VERBOSE, f"crunching test data into dict")
-    test = {
-        'opt': opt,
-        'comptime': t2 - t1,
-        'size': stat(file_name).st_size,
-        'file': file_name,
-        'runtime': t4 - t3
-    }
-    log(VERBOSE, f"crunched dict:\n{test}")
-    return test
-
-def test_2_pretty(test: dict) -> str:
-    return f"""
-Optimization : {test['opt']}
-Compile Time : {test['comptime']} seconds
-Size         : {test['size']} bytes
-File         : {test['file']}
-Run Time     : {test['runtime']} seconds
-    """
-
 # header slapped at the top of each log
 log_header = f"""
 {'='*80}
@@ -245,16 +224,126 @@ READING BYTES FROM {path.basename(args.file)}
 {'='*80}
 """
 
+class TestHandler:
+    #==--   --==#
+    #  COMPILE  # ~ quick
+    #==--   --==#
+    @staticmethod
+    def quick_compile():
+        """ should not be used for tracking comptime """
+        start = time()
+        with ProcessPoolExecutor() as executor:
+            compile_futures = {executor.submit(TestHandler.compile, BASE_COMPILE_COMMAND + [path.join(args.bin_out, f"hexdump-{opt}"), f"-O{opt}"]): opt for opt in OPTIMIZATION_LEVELS}
+
+            for future in as_completed(compile_futures):
+                opt = compile_futures[future]
+                try:
+                    t1, t2 = future.result()
+                    log(INFO, f"Compiled binary... took {t2 - t1} seconds")
+                except Exception as exc:
+                    log(ERORR, f"An error occurred while compiling a binary: {exc}")
+        end = time()
+        log(VERBOSE, "Final time taken:", end-start)
+
+    #==--   --==#
+    #  COMPILE  #
+    #==--   --==#
+    @staticmethod
+    def compile(compile_command):
+        log(VERBOSE, f"compile_command = {compile_command}")
+        start = time()
+        log(VERBOSE, "running compile_command")
+        try:
+            sp.call(compile_command, stdout=sp.DEVNULL, stderr=sp.STDOUT)
+        except Exception as exc:
+            log(ERROR, f"An error occurred while compiling: {exc}")
+        else:
+            log(VERBOSE, "compile_command executed")
+        end = time()
+        
+        return (start, end)
+    
+    #==-- --==#
+    #   RUN   #
+    #==-- --==#
+    @staticmethod
+    def run(run_command):
+        log(VERBOSE, f"run_command = {run_command}")
+        start = time()
+        log(VERBOSE, "running run_command")
+        sp.call(run_command, stdout=sp.DEVNULL, stderr=sp.STDOUT)
+        log(VERBOSE, "run_command executed")
+        end = time()
+    
+        return (start, end)
+    
+    #==--  --==#
+    # RUN TEST #
+    #==--  --==#
+    @staticmethod
+    def run_test(opt):
+        start = time()
+        file_name = path.join(args.bin_out, f"hexdump-{opt}")
+        log(VERBOSE, f"file_name = {file_name}")
+
+        #----------------#
+        # COMPILE BINARY #
+        #----------------#
+        compile_command = BASE_COMPILE_COMMAND + [file_name, f"-O{opt}"]
+        t1, t2 = TestHandler.compile(compile_command)
+
+        #------------#
+        # RUN BINARY #
+        #------------#
+        run_command = [file_name] + TEST_ARGS
+        t3, t4 = TestHandler.run(run_command)
+
+        log(VERBOSE, f"crunching test data into dict")
+        test = {
+            'opt': opt,
+            'comptime': t2 - t1,
+            'size': stat(file_name).st_size,
+            'file': file_name,
+            'runtime': t4 - t3
+        }
+        log(VERBOSE, f"crunched dict:\n{test}")
+        end = time()
+        log(VERBOSE, "Final time taken:", end-start)
+
+        return test
+
+def test_2_pretty(test: dict) -> str:
+    return f"""
+Optimization : {test['opt']}
+Compile Time : {test['comptime']} seconds
+Size         : {test['size']} bytes
+File         : {test['file']}
+Run Time     : {test['runtime']} seconds
+    """
+
+############
+# TEST ALL #
+############
+if args.test_all:
+    with open(args.log, 'a') as f:
+        f.write(log_header)
+
+        for idx, opt in enumerate(OPTIMIZATION_LEVELS):
+            f.write(test_2_pretty(TestHandler.run_test(opt)))
+            log(INFO, f"Finished test {idx + 1} (optimization: {opt})")
+        
+        f.write(f"\n\n{'-'*80}\n\n")
+
 
 #########
 # CLEAN #
 #########
 if args.clean:
     log(VERBOSE, "searching for test binaries")
-    binaries = [f for f in listdir(args.bin_out) if any("hexdump-"+opt in f for opt in optimization)]
+    binaries = [f for f in listdir(args.bin_out) if any("hexdump-"+opt in f for opt in OPTIMIZATION_LEVELS)]
     if len(binaries) == 0:
         log(VERBOSE, "no test binaries")
-        exit(0)
+        sys.exit(0)
 
     log(VERBOSE, f"found {binaries}")
 
@@ -263,7 +352,7 @@ if args.clean:
         remove(path.join(args.bin_out, bin))
         log(VERBOSE, "removed")
     
-    exit(0)
+    sys.exit(0)
 
 ###################
 # COMPUTE AVERAGE #
@@ -273,9 +362,11 @@ if args.compavg:
 
     tests = []
 
-    for _ in range(args.compavg):
-        for opt in optimization:
-            tests.append(run_test(opt))
+    log(INFO, f"0/{args.compavg} tests...")
+    for i in range(args.compavg):
+        for opt in OPTIMIZATION_LEVELS:
+            tests.append(TestHandler.run_test(opt))
+        log(INFO, f"{i+1}/{args.compavg} Done")
 
     log(VERBOSE, "sorting tests by optimization")
     lists_dict = {}
@@ -299,7 +390,7 @@ if args.compavg:
     total_avg_runtime = sum(opt_dict['avg_runtime'] for opt_dict in avg_dict.values()) / len(avg_dict)
 
     log(VERBOSE, "writing to log file")
-    with open(args.log, "a") as f:
+    with open(args.log, 'a') as f:
         f.write(log_header+f"COMPAVG OVER THE SPAN OF {args.compavg} TESTS\n\n")
 
         for opt, opt_dict in avg_dict.items():
@@ -307,19 +398,44 @@ if args.compavg:
         f.write(f"Overall:\navg_comptime: {total_avg_comptime}\navg_runtime: {total_avg_runtime}\n")
 
         f.write(f"\n\n{'-'*80}\n\n")
-    exit(0)
+    sys.exit(0)
 
-############
-# TEST ALL #
-############
-if args.test_all:
-    with open(args.log, "a") as f:
-        f.write(log_header)
+###############
+# AVG RUNTIME #
+###############
+if args.avg_runtime:
+    TestHandler.quick_compile()
 
-        for idx, opt in enumerate(optimization):
-            f.write(test_2_pretty(run_test(opt)))
-            log(INFO, f"Finished test {idx + 1} (optimization: {opt})")
+    log(VERBOSE, "calulate and sort runtimes")
+    runtimes = {}
+    for _ in range(args.avg_runtime):
+        for opt in OPTIMIZATION_LEVELS:
+            file_name = path.join(args.bin_out, f"hexdump-{opt}")
+            t1, t2 = TestHandler.run([file_name] + TEST_ARGS)
+            runtimes.setdefault(opt, []).append(t2 - t1)
+
+    avg_dict = {}
+    log(VERBOSE, "get runtime averages by optimization")
+    for opt, opt_runtimes in runtimes.items():
+        log(VERBOSE, f"opt = {opt}")
+        avg_runtime = sum(runtime for runtime in opt_runtimes) / len(opt_runtimes)
+        log(VERBOSE, f"avg_runtime = {avg_runtime}")
+        log(VERBOSE, "Crunching data into dict & adding it to avg_dict")
+        avg_dict[opt] = {'avg_runtime': avg_runtime}
         
-        f.write(f"\n\n{'-'*80}\n\n")
-    exit(0)
+    log(VERBOSE, "computing the average comptime and runtime across all optimizations")
+    total_avg_runtime = sum(opt_dict['avg_runtime'] for opt_dict in avg_dict.values()) / len(avg_dict)
 
+    log(VERBOSE, "writing to log file")
+    with open(args.log, 'a') as f:
+        f.write(log_header+f"AVG_RUNTIME OVER THE SPAN OF {args.avg_runtime} TESTS\n\n")
+
+        for opt, opt_dict in avg_dict.items():
+            f.write(f"Optimization: {opt}\navg_runtime: {opt_dict['avg_runtime']}\n\n")
+        f.write(f"Overall:\navg_runtime: {total_avg_runtime}\n")
+
+        f.write(f"\n\n{'-'*80}\n\n")
+    
+    sys.exit(0)
+
+log(WARNING, "Please supply a test to execute!")
