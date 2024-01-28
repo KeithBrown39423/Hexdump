6c9723a41e925d494b35f1aa49d532fc44b3267c
Keith Brown <keithbrown39423@outlook.com>
2024-01-27 23:14:11 -0600

    :building_construction: Clean up build

--- build.zig
+++ build.zig
@@ -5,26 +5,11 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{
         .preferred_optimize_mode = std.builtin.Mode.ReleaseFast
     });
-    const exe = b.addExecutable(.{
+    const main = b.addExecutable(.{
         .name = "hexdump",
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
     });
-    b.installArtifact(exe);
-    const run_cmd = b.addRunArtifact(exe);
-    run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-    const unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-    const run_unit_tests = b.addRunArtifact(unit_tests);
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_unit_tests.step);
+    b.installArtifact(main);
 }
