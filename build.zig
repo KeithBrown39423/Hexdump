8b718aa765efe7ec0ef80baa49212f7b392aaf22
KeithBrown39423 <KeithBrown39423@outlook.com>
2024-06-05 17:13:18 -0500

    🏗️ Change executable name

--- build.zig
+++ build.zig
@@ -10,7 +10,7 @@ pub fn build(b: *std.Build) void {
     });
 
     const exe = b.addExecutable(.{
-        .name = "Hexdump",
+        .name = "hexdump",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
